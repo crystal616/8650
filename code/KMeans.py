d98fe151295f9e2b2b8245b2bb1f8c2af8bade79
Ying Cai <cai7@g.clemson.edu>
2019-03-01 12:56:46 -0500

    Update KMeans.py

--- code/KMeans.py
+++ code/KMeans.py
@@ -16,9 +16,9 @@ numOfVariable=74
 #         g.append(random.randint(1, 100))
 #     return g
 
-def kmeans(clusters, data):
+def kmeans(clusters, data, weight):
     t0 = time()
-    KM = KMeans(n_clusters=clusters).fit(data)
+    KM = KMeans(n_clusters=clusters).fit(data, sample_weight=weight)
     usedtime=time()-t0
     return KM, usedtime
 
@@ -33,21 +33,48 @@ with open(filename) as fn:
         for value in data:
             vlist.append(float(value))
         dataset.append(vlist)
-  
+   
 fn.close()
 # dataset=[]
 # for _i in range(1000):
 #     dataset.append(generate(10))
 
 clusters=[100,500]
-
+print("done")
 for k in clusters:
-    totalKM, totalT = kmeans(k, dataset)
+    totalKM, totalT = kmeans(k, dataset, None)
     labels = totalKM.labels_
-    with open("Bio_train\\Total set k="+str(k)+".txt",'w',encoding='utf-8') as f:
+    with open("Bio_train\\Full set k="+str(k)+".txt",'w',encoding='utf-8') as f:
         f.write("used time: "+str(totalT)+"\n# of Clusters:"+str(k)+"\nCluster\n")
         for item in labels:
             f.write(str(item)+'\n')    
         f.close()
-    
 
+sampleSize=[1000,2000,5000]
+repeat=[1,2,3]
+ 
+for s in sampleSize:
+    for r in repeat:
+        subData=[]
+        weights=[]
+        with open("Bio_train\\Bio_train "+str(s)+" "+str(r)+".txt") as f:
+            for line in f:
+                l=line.split('\t')
+                index=int(l[0])
+                weight=float(l[1].strip())
+                subData.append(dataset[index])
+                weights.append(weight)
+            print("done")
+            for k in clusters:
+                predictKM, pTime = kmeans(k, subData, weights)
+                predict_lable = predictKM.predict(dataset)
+                with open("Bio_train\\Prediction "+str(s)+" r="+str(r)+" k="+str(k)+".txt",'w',encoding='utf-8') as f:
+                    f.write("used time: "+str(pTime)+"\n# of Clusters:"+str(k)+"\nCluster\n")
+                    for item in predict_lable:
+                        f.write(str(item)+'\n')    
+                    f.close()                    
+                    
+                    
+                    
+                    
+                    
